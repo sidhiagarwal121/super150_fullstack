78cf7a84bac23b53b7d3b902a2e297c6b828ae00
sidhiagarwal121 <87852812+sidhiagarwal121@users.noreply.github.com>
2022-09-21 10:41:12 +0530

    Add files via upload

---
+++ day_06.js
@@ -0,0 +1,21 @@
+// function first()
+// {
+//     console.log('first');
+//     second();
+// }
+// function second()
+// {
+//     console.log('second');
+//     third();
+// }
+// function third()
+// {
+//     console.log('third');
+// }
+// first();
+// // second();
+// // third();
+alert("hello world");
+[1,2].forEach(alert);
+let age=5;
+console.log(age);
